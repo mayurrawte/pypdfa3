83e4eb60c8471e85f8d2c5a287af9fe308e99583
mayurrawte <mayur@shipthis.co>
2023-09-16 13:50:03 +0530

    Cors added

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from fastapi import FastAPI, UploadFile, File
 from fastapi.responses import FileResponse
+from fastapi.middleware.cors import CORSMiddleware
+
 import tempfile
 import secrets
 import subprocess
@@ -8,6 +10,15 @@ import os
 from utils import save_temp_file, create_post_script_file
 
 app = FastAPI()
+origins = ['*']
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 
